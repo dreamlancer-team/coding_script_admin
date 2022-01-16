fc78c93ba75733b3579c2fac0b4a46831a7a4ffa
unknown <aniketkhote99@gmail.com>
2022-01-16 15:52:28 +0530

    admin controller and scaffold created

---
+++ resources/views/layout/styles.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />		
+<link href="http://assets.dreamlancer.in/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+<link href="http://assets.dreamlancer.in/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+
+@stack('styles')
